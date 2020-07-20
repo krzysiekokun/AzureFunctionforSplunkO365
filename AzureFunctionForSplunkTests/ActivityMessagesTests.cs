94a7fed0202bac374d9d6b4566b135603defa246
Adam Jagocki <ajagocki@hotmail.com>
2020-07-20 23:43:12 +0200

    added unit test project

---
+++ AzureFunctionForSplunkTests/ActivityMessagesTests.cs
@@ -0,0 +1,25 @@
+using AzureFunctionForSplunk;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace AzureFunctionForSplunkTests
+{
+
+    [TestClass]
+    public class ActivityMessagesTests
+    {
+        [TestMethod]
+        [DeploymentItem(@"message1.txt", "optionalOutFolder")]
+        public void CheckIncommingMessageCorrectStructure()
+        {
+            var azMonMsgs = (AzMonMessages)Activator.CreateInstance(typeof(ActivityLogMessages), TestUtils.CreateLogger(LoggerTypes.Null));
+            var result = azMonMsgs.DecomposeIncomingBatch(new string[] { File.ReadAllText("message1.txt") });
+
+
+        }
+
+        
+    }
+}
---
+++ AzureFunctionForSplunkTests/TestUtils.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctionForSplunkTests
+{
+    public class TestUtils
+    {
+        public static ILogger CreateLogger(LoggerTypes type = LoggerTypes.Null)
+        {
+            ILogger logger;
+
+            if (type == LoggerTypes.List)
+            {
+                logger = new ListLogger();
+            }
+            else
+            {
+                logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+            }
+
+            return logger;
+        }
+    }
+}
