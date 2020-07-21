a290928b555ee3a3ef6da739b62344d6e8759958
Adam Jagocki <ajagocki@hotmail.com>
2020-07-21 17:13:39 +0200

    improved logging

--- AzureFunctionForSplunk/EhWadTelemetryExt.cs
+++ AzureFunctionForSplunk/EhWadTelemetryExt.cs
@@ -33,17 +33,17 @@ namespace AzureFunctionForSplunk
 {
     public static class EhWadTelemetryExt
     {
-        [FunctionName("EhWadTelemetryExt")]
-        public static async Task Run(
-            [EventHubTrigger("%input-hub-name-wad%", Connection = "hubConnection", ConsumerGroup = "%consumer-group-wad%")]string[] messages,
-            [EventHub("%output-hub-name-proxy%", Connection = "outputHubConnection")]IAsyncCollector<string> outputEvents,
-            IBinder blobFaultBinder,
-            IBinder incomingBatchBinder,
-            Binder queueFaultBinder,
-            ILogger log)
-        {
-            var runner = new Runner();
-            await runner.Run<WadMessages, WadSplunkEventMessages>(messages, blobFaultBinder, queueFaultBinder, incomingBatchBinder, outputEvents, log);
-        }
+        //[FunctionName("EhWadTelemetryExt")]
+        //public static async Task Run(
+        //    [EventHubTrigger("%input-hub-name-wad%", Connection = "hubConnection", ConsumerGroup = "%consumer-group-wad%")]string[] messages,
+        //    [EventHub("%output-hub-name-proxy%", Connection = "outputHubConnection")]IAsyncCollector<string> outputEvents,
+        //    IBinder blobFaultBinder,
+        //    IBinder incomingBatchBinder,
+        //    Binder queueFaultBinder,
+        //    ILogger log)
+        //{
+        //    var runner = new Runner();
+        //    await runner.Run<WadMessages, WadSplunkEventMessages>(messages, blobFaultBinder, queueFaultBinder, incomingBatchBinder, outputEvents, log);
+        //}
     }
 }
--- AzureFunctionForSplunk/FaultProcessor.cs
+++ AzureFunctionForSplunk/FaultProcessor.cs
@@ -12,55 +12,55 @@ namespace AzureFunctionForSplunk
 {
     public static class FaultProcessor
     {
-        [FunctionName("FaultProcessor")]
-        public static async Task Run(
-            [QueueTrigger("%input-hub-name-faults%", Connection = "AzureWebJobsStorage")]string fault,
-            [EventHub("%output-hub-name-proxy%", Connection = "outputHubConnection")]IAsyncCollector<string> outputEvents,
-            IBinder blobFaultBinder,
-            ILogger log)
-        {
-            string outputBinding = Utils.getEnvironmentVariable("outputBinding").ToLower();
-            if (outputBinding.Length == 0)
-            {
-                log.LogError("Value for outputBinding is required. Permitted values are: 'proxy', 'hec', 'eventhub'.");
-                return;
-            }
+        //[FunctionName("FaultProcessor")]
+        //public static async Task Run(
+        //    [QueueTrigger("%input-hub-name-faults%", Connection = "AzureWebJobsStorage")]string fault,
+        //    [EventHub("%output-hub-name-proxy%", Connection = "outputHubConnection")]IAsyncCollector<string> outputEvents,
+        //    IBinder blobFaultBinder,
+        //    ILogger log)
+        //{
+        //    string outputBinding = Utils.getEnvironmentVariable("outputBinding").ToLower();
+        //    if (outputBinding.Length == 0)
+        //    {
+        //        log.LogError("Value for outputBinding is required. Permitted values are: 'proxy', 'hec', 'eventhub'.");
+        //        return;
+        //    }
 
-            var faultData = JsonConvert.DeserializeObject<TransmissionFaultMessage>(fault);
+        //    var faultData = JsonConvert.DeserializeObject<TransmissionFaultMessage>(fault);
 
-            var blobReader = await blobFaultBinder.BindAsync<CloudBlockBlob>(
-                    new BlobAttribute($"transmission-faults/{faultData.id}", FileAccess.ReadWrite));
+        //    var blobReader = await blobFaultBinder.BindAsync<CloudBlockBlob>(
+        //            new BlobAttribute($"transmission-faults/{faultData.id}", FileAccess.ReadWrite));
 
-            var json = await blobReader.DownloadTextAsync();
+        //    var json = await blobReader.DownloadTextAsync();
 
-            try
-            {
-                List<string> faultMessages = await Task<List<string>>.Factory.StartNew(() => JsonConvert.DeserializeObject<List<string>>(json));
+        //    try
+        //    {
+        //        List<string> faultMessages = await Task<List<string>>.Factory.StartNew(() => JsonConvert.DeserializeObject<List<string>>(json));
 
-                switch (outputBinding)
-                {
-                    case "hec":
-                        await Utils.obHEC(faultMessages, log);
-                        break;
-                    case "proxy":
-                        await Utils.obProxy(faultMessages, log);
-                        break;
-                    case "eventhub":
-                        await Utils.obEventhub(faultMessages, outputEvents, log);
-                        break;
-                }
+        //        switch (outputBinding)
+        //        {
+        //            case "hec":
+        //                await Utils.obHEC(faultMessages, log);
+        //                break;
+        //            case "proxy":
+        //                await Utils.obProxy(faultMessages, log);
+        //                break;
+        //            case "eventhub":
+        //                await Utils.obEventhub(faultMessages, outputEvents, log);
+        //                break;
+        //        }
 
-            }
-            catch (Exception ex)
-            {
-                log.LogError(ex.Message);
-                log.LogError($"FaultProcessor failed to transmit: {faultData.id}");
-                throw new Exception("FaultProcessor failed to transmit");
-            }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        log.LogError(ex.Message);
+        //        log.LogError($"FaultProcessor failed to transmit: {faultData.id}");
+        //        throw new Exception("FaultProcessor failed to transmit");
+        //    }
 
-            await blobReader.DeleteAsync();
+        //    await blobReader.DeleteAsync();
 
-            log.LogInformation($"C# Queue trigger function FaultProcessor processed: {faultData.id}");
-        }
+        //    log.LogInformation($"C# Queue trigger function FaultProcessor processed: {faultData.id}");
+        //}
     }
 }
