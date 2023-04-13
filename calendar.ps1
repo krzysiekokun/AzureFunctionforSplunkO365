6f486c68e6cca410a7fc535a602afe80a91a9aa8
krzysiekokun <98360702+krzysiekokun@users.noreply.github.com>
2023-04-13 19:24:41 +0200

    Add files via upload

---
+++ calendar.ps1
@@ -0,0 +1,67 @@
+# Uwierzytelnienie z aplikacją zarejestrowaną
+$tenantId = "9be8208b-1b85-4ac6-ac80-bbe2d6889d37"
+$appId = "f3dc1fe5-d774-4ac0-960f-67138f65a42d"
+$clientSecretString = "7H_8Q~xfYahdoMGiSdqNGwAKfsMA2J0Q8tgP2bRg"
+$secureClientSecret = ConvertTo-SecureString -String $clientSecretString -AsPlainText -Force
+
+# Instalacja modułu MSAL.PS
+if (-not (Get-Module -ListAvailable -Name "MSAL.PS")) {
+    Install-Module -Name MSAL.PS -Scope CurrentUser -Force
+}
+
+# Importowanie modułu MSAL.PS
+Import-Module MSAL.PS
+
+# Uzyskanie tokena dostępu
+$authority = "https://login.microsoftonline.com/$tenantId"
+$scope = "https://graph.microsoft.com/.default"
+$token = Get-MsalToken -ClientId $appId -ClientSecret $secureClientSecret -Authority $authority -Scope $scope
+
+# Użycie tokena dostępu do wywołania API Graph
+$headers = @{
+    "Authorization" = "Bearer $($token.AccessToken)"
+    "Content-Type"  = "application/json"
+}
+
+# Pobierz listę wszystkich użytkowników
+$usersUri = "https://graph.microsoft.com/v1.0/users"
+$usersResponse = Invoke-RestMethod -Uri $usersUri -Headers $headers -Method Get
+$users = $usersResponse.value
+
+# Dodaj przedział czasowy dla createdDateTime
+$createdStartDate = Get-Date -Year 2023 -Month 3 -Day 1
+$createdEndDate = Get-Date -Year 2023 -Month 4 -Day 30
+
+# Wypisz wyniki w formacie JSON
+$allResults = @()
+
+foreach ($user in $users) {
+    $userId = $user.id
+    $eventsUri = "https://graph.microsoft.com/v1.0/users/$userId/calendar/calendarView?startDateTime=$($createdStartDate.ToString("2022-04-11T10:00:00-08:00"))&endDateTime=$($createdEndDate.ToString("2024-04-13T18:00:00-08:00"))"
+    $eventsResponse = Invoke-RestMethod -Uri $eventsUri -Headers $headers -Method Get
+
+    foreach ($event in $eventsResponse.value) {
+        $selectedValues = $event | Select-Object -Property @{
+                Name = 'organizer'
+                Expression = {$_.organizer.emailAddress.name}
+            },
+            subject,
+            @{Name='recurrence'; Expression={$_.recurrence.pattern.type}},
+            @{Name='start'; Expression={$_.start.dateTime}},
+            @{Name='end'; Expression={$_.end.dateTime}},
+            createdDateTime, lastModifiedDateTime,
+            @{Name='attendees'; Expression={$_.attendees.Count}}
+        $allResults += $selectedValues
+    }
+}
+
+# Sortuj wyniki według liczby uczestników (attendeesCount) w kolejności malejącej
+$sortedResults = $allResults | Sort-Object -Property attendees -Descending
+
+
+# Zapisz wynik do pliku JSON
+$jsonResults = $sortedResults | ConvertTo-Json
+$jsonFilePath = "results.json"
+Set-Content -Path $jsonFilePath -Value $jsonResults
+
+Write-Host "Wyniki zapisane do pliku: $jsonFilePath"
