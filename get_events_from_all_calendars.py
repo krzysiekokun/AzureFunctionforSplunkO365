222694767bb3a32ff0ccdcc9b327dc591b084423
krzysiekokun <98360702+krzysiekokun@users.noreply.github.com>
2023-04-25 00:42:53 +0200

    Add files via upload

--- get_events_from_all_calendars.py
+++ get_events_from_all_calendars.py
@@ -24,23 +24,19 @@ def get_access_token(client_id, client_secret, tenant_id):
         print("Error:", response.status_code, response.text)
         return None
 
-def get_users(access_token, users_file):
-    users = []
-    with open(users_file, 'r', encoding='utf-8') as file:
-        for line in file:
-            user_identifier = line.strip()
-            url = f'https://graph.microsoft.com/v1.0/users/{user_identifier}'
-            headers = {
-                'Authorization': 'Bearer ' + access_token,
-                'Content-Type': 'application/json',
-                'Accept-Charset': 'utf-8'
-            }
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                users.append(response.json())
-            else:
-                print("Error:", response.status_code, response.text)
-    return users
+def get_user(access_token, user_identifier):
+    url = f'https://graph.microsoft.com/v1.0/users/{user_identifier}'
+    headers = {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json',
+        'Accept-Charset': 'utf-8'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
 
 def get_all_events(access_token, user_id):
     url = f'https://graph.microsoft.com/v1.0/users/{user_id}/events'
@@ -63,33 +59,37 @@ def save_to_csv(user_name, recurring_events, output_file):
     with open(output_file, 'a', newline='', encoding='utf-8') as file:
         csv_writer = csv.writer(file)
         for event in recurring_events:
+            organizer = event['organizer']['emailAddress']['name'] if 'organizer' in event and 'emailAddress' in event['organizer'] and 'name' in event['organizer']['emailAddress'] else 'N/A'
             recurrence_pattern = event['recurrence']['pattern'] if event['recurrence'] and 'pattern' in event['recurrence'] else 'N/A'
             csv_writer.writerow([
                 user_name,
                 event['subject'],
                 event['start']['dateTime'],
                 event['end']['dateTime'],
-                recurrence_pattern
+                recurrence_pattern,
+                organizer
             ])
 
 # Dodaj nagłówek do pliku CSV przed rozpoczęciem zapisywania danych
 with open(output_file, 'w', newline='', encoding='utf-8') as file:
     csv_writer = csv.writer(file)
-    csv_writer.writerow(['User', 'Event Subject', 'Start Time', 'End Time', 'Recurrence Pattern'])
+    csv_writer.writerow(['User', 'Event Subject', 'Start Time', 'End Time', 'Recurrence Pattern', 'Organizer'])
 
 access_token = get_access_token(client_id, client_secret, tenant_id)
 if access_token:
-    users = get_users(access_token, users_file)
-    for user in users:
-        user_id = user['id']
-        user_display_name = user['displayName']
-        
-        all_events = get_all_events(access_token, user_id)
-        recurring_events = filter_recurring_events(all_events)
+    with open(users_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            user_identifier = line.strip()
+            user = get_user(access_token, user_identifier)
+            if user:
+                user_id = user['id']
+                user_display_name = user['displayName']
 
-        save_to_csv(user_display_name, recurring_events, output_file)
+                all_events = get_all_events(access_token, user_id)
+                recurring_events = filter_recurring_events(all_events)
+
+                save_to_csv(user_display_name, recurring_events, output_file)
 
     print(f'Zapisano cykliczne spotkania do pliku {output_file}')
 else:
     print("Nie można uzyskać tokena dostępu.")
-
