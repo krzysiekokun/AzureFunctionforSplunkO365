77d844a00ff2061497afbbc73b3fa7b5c0ea43c6
krzysiekokun <98360702+krzysiekokun@users.noreply.github.com>
2023-04-25 22:17:52 +0200

    Add files via upload

--- get_events_to_csv.py
+++ get_events_to_csv.py
@@ -1,12 +1,12 @@
 import requests
-import json
+import csv
 from datetime import datetime
 
 client_id = 'f3dc1fe5-d774-4ac0-960f-67138f65a42d'
 client_secret = '7H_8Q~xfYahdoMGiSdqNGwAKfsMA2J0Q8tgP2bRg'
 tenant_id = '9be8208b-1b85-4ac6-ac80-bbe2d6889d37'
 users_file = 'users.txt'
-output_file = 'all_events.json'
+output_file = 'filtered_events.csv'
 
 def get_access_token(client_id, client_secret, tenant_id):
     url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
@@ -26,7 +26,7 @@ def get_access_token(client_id, client_secret, tenant_id):
 
 def get_users(access_token, users_file):
     users = []
-    with open(users_file, 'r', encoding='utf-8-sig') as file:
+    with open(users_file, 'r', encoding='utf-8') as file:
         for line in file:
             user_identifier = line.strip()
             url = f'https://graph.microsoft.com/v1.0/users/{user_identifier}'
@@ -43,27 +43,18 @@ def get_users(access_token, users_file):
     return users
 
 def get_all_events(access_token, user_id):
-    events = []
     url = f'https://graph.microsoft.com/v1.0/users/{user_id}/events'
     headers = {
         'Authorization': 'Bearer ' + access_token,
         'Content-Type': 'application/json',
-        'Accept-Charset': 'utf-8-sig'
+        'Accept-Charset': 'utf-8'
     }
-
-    while url:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            json_response = response.json()
-            events.extend(json_response['value'])
-
-            # Sprawdzanie czy istnieje kolejna strona wyników
-            url = json_response.get('@odata.nextLink', None)
-        else:
-            print("Error:", response.status_code, response.text)
-            return []
-
-    return events
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['value']
+    else:
+        print("Error:", response.status_code, response.text)
+        return []
 
 def save_to_csv(user_email, events, output_file):
     with open(output_file, 'a', newline='', encoding='utf-8') as file:
