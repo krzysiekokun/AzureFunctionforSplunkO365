f4cc443fb18cd77342b828f2c041d4ce1724e9c1
krzysiekokun <98360702+krzysiekokun@users.noreply.github.com>
2023-04-26 18:06:33 +0200

    Add files via upload

--- get_events_to_csv.py
+++ get_events_to_csv.py
@@ -92,84 +92,79 @@ def get_all_events(access_token, user_id, start_date, end_date):
     
     return events
 
-
-
-def recurrence_pattern_to_string(pattern_type, pattern_interval, pattern_days_of_week, range_type):
-    return f"{pattern_type} (Interval: {pattern_interval}, Days of Week: {pattern_days_of_week}, Range Type: {range_type})"
-
 def save_to_csv(user_email, events, output_file):
-    with open(output_file, 'a', newline='', encoding='utf-8') as file:
+    with open(output_file, 'a', newline='', encoding='utf-8-sig') as file:
         csv_writer = csv.writer(file, delimiter=';')
         for event in events:
+            organizer_name = event['organizer']['emailAddress']['name']
+            subject = event['subject']
+            attendees_count = len(event['attendees'])
+            is_organizer = event['isOrganizer']
+            created_date_time = event['createdDateTime']
+            last_modified_date_time = event['lastModifiedDateTime']
+            start_date_time = event['start']['dateTime']
+            end_date_time = event['end']['dateTime']
+            is_cancelled = event['isCancelled']
+            is_all_day = event['isAllDay']
+            importance = event['importance']
+
             recurrence = event.get('recurrence', None)
-            if recurrence is not None:
+            if recurrence:
                 pattern_type = recurrence['pattern']['type']
                 pattern_interval = recurrence['pattern']['interval']
                 pattern_days_of_week = recurrence['pattern'].get('daysOfWeek', '')
                 range_type = recurrence['range']['type']
+            else:
+                pattern_type = ''
+                pattern_interval = ''
+                pattern_days_of_week = ''
+                range_type = ''
 
-                recurrence_pattern_str = recurrence_pattern_to_string(pattern_type, pattern_interval, pattern_days_of_week, range_type)
-
-                organizer_name = event['organizer']['emailAddress']['name']
-                subject = event['subject']
-                attendees_count = len(event['attendees'])
-                is_organizer = user_email == event['organizer']['emailAddress']['address']
-                created_date_time = event['createdDateTime']
-                last_modified_date_time = event['lastModifiedDateTime']
-                start_date_time = event['start']['dateTime']
-                end_date_time = event['end']['dateTime']
-                is_cancelled = event['isCancelled']
-                is_all_day = event['isAllDay']
-                importance = event['importance']
-
-                csv_writer.writerow([
-                    organizer_name,
-                    subject,
-                    attendees_count,
-                    is_organizer,
-                    recurrence_pattern_str,
-                    created_date_time,
-                    last_modified_date_time,
-                    start_date_time,
-                    end_date_time,
-                    is_cancelled,
-                    is_all_day,
-                    importance
-                ])
+            row = [user_email, organizer_name, subject, attendees_count, is_organizer, pattern_type, pattern_interval, pattern_days_of_week, range_type, created_date_time, last_modified_date_time, start_date_time, end_date_time, is_cancelled, is_all_day, importance]
+            csv_writer.writerow(row)
 
 with open(output_file, 'w', newline='', encoding='utf-8-sig') as file:
     csv_writer = csv.writer(file, delimiter=';')
-    csv_writer.writerow(['Organizer Name', 'Subject', 'Attendees Count', 'Is Organizer', 'Recurrence Pattern', 'Created Date Time', 'Last Modified Date Time', 'Start Date Time', 'End Date Time', 'Is Cancelled', 'Is All Day', 'Importance'])
+    csv_writer.writerow(['User Email', 'Organizer Name', 'Subject', 'Attendees Count', 'Is Organizer', 'Pattern Type', 'Pattern Interval', 'Pattern Days of Week', 'Range Type', 'Created Date Time', 'Last Modified Date Time', 'Start Date Time', 'End Date Time', 'Is Cancelled', 'Is All Day', 'Importance'])
 
 access_token, token_received_time, token_expires_in = get_access_token(client_id, client_secret, tenant_id)
-
 if access_token:
     users = get_users(access_token, users_file)
+    failed_users = []
 
     for user in users:
         user_id = user['id']
         user_email = user['mail']
 
         print(f'Processing events for user: {user_email}')
-        user_events = []
+        total_events = 0
 
         for start_date, end_date in date_ranges:
-            print(f'Fetching events between {start_date} and {end_date}...')
-
             if is_token_expired(token_received_time, token_expires_in):
                 access_token, token_received_time, token_expires_in = get_access_token(client_id, client_secret, tenant_id)
+                if not access_token:
+                    print("Failed to refresh access token. Exiting.")
+                    break
 
-            events = get_all_events(access_token, user_id, start_date, end_date)
-            user_events.extend(events)
-            print(f'Fetched {len(events)} events for user {user_email} between {start_date} and {end_date}')
+            try:
+                events = get_all_events(access_token, user_id, start_date, end_date)
+                save_to_csv(user_email, events, output_file)
+                total_events += len(events)
+                print(f'Fetched {len(events)} events for user {user_email} between {start_date} and {end_date}')
 
-        print(f'Saving events to the output file {output_file}...')
-        save_to_csv(user_email, user_events, output_file)
+            except Exception as e:
+                print(f"Error processing events for user {user_email}: {e}")
+                failed_users.append(user_email)
+                break
 
-        recurring_events_count = sum(1 for event in user_events if event.get('recurrence', None) is not None)
-        print(f'Total events for user {user_email}: {len(user_events)} (recurring events: {recurring_events_count})')
-        print(f'Finished processing events for user {user_email}\n')
+        print(f'Finished processing events for user: {user_email}. Total events: {total_events}')
 
-    print(f'Saved selected event data to file {output_file}')
+    with open('failed_users.txt', 'w', encoding='utf-8') as file:
+        for email in failed_users:
+            file.write(f"{email}\n")
+
+    print(f"Finished processing all users. Failed users: {len(failed_users)}")
 else:
-    print("Cannot obtain access token.")
+    print("Failed to obtain access token.")
+
+
